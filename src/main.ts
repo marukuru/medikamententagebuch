6e8c0903488253e465f67f2b9fbb1c8f857af045
marukuru <marukuru@users.noreply.github.com>
2026-01-20 20:33:31 +0100

    fix: Enable ZoneChangeDetection for build
    ZoneChangeDetection is required for the application to build correctly. This commit re-enables it.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideZonelessChangeDetection } from '@angular/core';
+// Hier muss provideZoneChangeDetection genutzt werden, sonst kommt es zu Build-Fehler!
+import { provideZoneChangeDetection } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZonelessChangeDetection(),
+    provideZoneChangeDetection(),
   ],
 }).catch((err) => console.error(err));
