fa8ae9854d21d779fd31d09d4fcf56634b8ed89c
marukuru <marukuru@gmx.net>
2026-01-22 10:58:35 +0100

    Read version number from package.json

--- src/components/info.component.ts
+++ src/components/info.component.ts
@@ -1,9 +1,12 @@
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
+import { NgOptimizedImage } from '@angular/common';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'info',
   standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './info.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,6 +14,6 @@ export class InfoComponent {
   translationService = inject(TranslationService);
   t = this.translationService.translations;
   
-  appVersion = '1.0.0';
+  appVersion = version;
   author = 'u/gabbergand0lf';
 }
